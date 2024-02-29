32f4195b84bc3bfb24b2fa3bab0d6834df9ef466
Ricki Potts <ricki.glp@gmail.com>
2024-02-29 08:39:17 -0800

    tasks 0-5 mandatories completed ready for testing

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+document.ready(function() {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+    }
+  });
+  const selectedAmenities = {};
+  $('input[type="checkbox"]').change(function() {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+    if (this.checked) {
+      selectedAmenities[amenityId] = amenityName;
+    } else {
+      delete selected Amenities[amenityId];
+      }
+    const amenityList = Object.values(selectedAmenities).join(', ');
+    $('#amenities-list').text(amenityList);
+  });
+});
